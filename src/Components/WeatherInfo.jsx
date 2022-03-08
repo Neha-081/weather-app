a61f8949d83c64fca9650f462981205c1901544f
Neha <nehaprajapati1150@gmail.com>
2022-03-09 01:33:16 +0530

    handlesearch and handlechange

--- src/Components/WeatherInfo.jsx
+++ src/Components/WeatherInfo.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function WeatherInfo() {
+    const initialState={
+        query:"",
+        data:[]
+    }
+    const [state,setState]=useState(initialState);
+
+    const handleChange=(e)=>{
+        setState({...state,query:e.target.value})
+    }
+    const handleSearch=()=>{
+        
+    }
   return (
-    <div>WeatherInfo</div>
+    <div>
+        <h1>Waether Information</h1>
+        <input placeholder='Enter City Name..' value={state.query} onChange={handleChange}/>
+        <button onClick={handleSearch}>Search</button>
+    </div>
   )
 }
 
